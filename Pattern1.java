7f2c2ac89ca2d0a5e51799374f2ad2b8e8748fd7
Saniya <saniyashaikh77546@gmail.com>
2025-03-28 21:31:14 +0530

    Patterns

---
+++ Pattern1.java
@@ -0,0 +1,13 @@
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        int n = 10;
+        int m = 5;
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
